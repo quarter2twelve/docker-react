0408ea9e9ffc17d08b138930537c8383b5dd6c3c
quarter2twelve <junonuki68@gmail.com>
2021-07-04 01:19:38 +0900

    modified App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We're gonna fly into the mystery 
+          My Aim Is True 
         </p>
         <a
           className="App-link"
